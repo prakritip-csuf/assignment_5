c8b4a46099b358f6efc16de117b04cd8cf19ea93
prakritip-csuf <prakritip@csu.fullerton.edu>
2025-05-01 16:03:31 -0700

    added name to globals.cpp

--- src/Globals.cpp
+++ src/Globals.cpp
@@ -3,5 +3,5 @@
 
 // Define the custom variables
 std::string customShapeName = "Custom Shape";
-std::string windowTitle = "CPSC 566 - Assignment 5 - <Your name here>";
+std::string windowTitle = "CPSC 566 - Assignment 5 - Prakriti Paudel";
 std::vector<std::string> jointName = { "Root", "Chest", "Waist", "Neck", "Right hip", "Right leg", "Right knee", "Right foot", "Left hip", "Left leg", "Left knee", "Left foot", "Right collarbone", "Right shoulder", "Right elbow", "Left collarbone", "Left shoulder", "Left elbow" };
